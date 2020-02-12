7e3fbb50f3472046c861df552ba08a575ab56eec
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2020-02-12 10:13:51 +0100

    change in pedigree

--- bped2/pedigree.py
+++ bped2/pedigree.py
@@ -5,31 +5,29 @@ import numpy as np
 
 class Pedigree:
     def __init__(self):
-        self.pedigree = {}
-        self.nb = 0
-    
+        self._pedigree = dict()
+        self._nb = 0
+
     def getPedigree(self):
-        return self.pedigree
+        return self._pedigree
 
     def getNB(self):
-        return self.nb
+        return self._nb
 
     def getPeople(self,idp):
-        return self.pedigree[idp]
+        return self._pedigree[idp]
 
     def load(self, fichier):
-        cpt = 0
         for i in open(fichier).readlines():
             tmp = i.strip().split()
             p = People(tmp[0],tmp[1],tmp[2],tmp[3])
             #print(p)
-            self.pedigree[cpt] = p
-            cpt+=1
-        self.nb = cpt
+            self._pedigree[tmp[1]] = p
+        self._nb = len(self._pedigree)
         #tab = [i.strip().split() for i in open(fichier).readlines()]
         #self.pedigree[nb] = People()
         #self.nb = len(self.p)
-    
+
     def sex(self):
         return
 
@@ -37,28 +35,30 @@ class Pedigree:
         return
 
     def addPeople(self,people):
-        cpt = self.nb
-        self.pedigree[cpt] = people
+        self._pedigree[people.pID] = people
+        self._nb = len(self._pedigree)
 
     def removePeople(self,idp):
-        del self.pedigree[idp]
+        del self._pedigree[idp]
 
     def __str__(self):
-        res = ""
-        for i in (self.pedigree.keys()):
-            res+=(self.pedigree[i].__str__()) + "\n"
-        return res
+        return ", ".join([str(v) for k,v in self._pedigree.items()])
 
 class People:
     def __init__(self,famID,pID,fatID,matID,sex=None):
-        self.famID = famID
-        self.pID = pID
-        self.fatID = fatID
-        self.matID = matID
-        self.sex = 0
-    
+        self._famID = famID
+        self._pID = pID
+        self._fatID = fatID
+        self._matID = matID
+        self._sex = 0
+
+    @property
     def get_famID(self):
-        return self.famID
+        return self._famID
+
+    def famID(self,famID):
+        #self._famID=famID
+        raise NameError("Cannot change FamID")
 
     def get_pID(self):
         return self.pID
@@ -72,25 +72,22 @@ class People:
     def get_sex(self):
         return self.sex
 
-    def set_famID(self,famID):
-        raise NameError("Cannot change FamID")
-
     def set_pID(self,pid):
         raise NameError("Cannot change pid")
 
     def set_fatID(self,fatID):
-        raise NameError("Cannot change FatID")   
+        raise NameError("Cannot change FatID")
 
     def set_matID(self,matID):
         raise NameError("Cannot change matID")
 
     def set_sex(self,sex):
-        self.sex = sex  
+        self.sex = sex
 
     def __str__(self):
-        return "[%s %s %s %s %s]"%(self.famID,self.pID,self.famID,self.famID,self.sex)
+        #return "[%s %s %s %s %s]"%(self._famID,self.pID,self.famID,self.famID,self.sex)
+        return f"{self._famID}"
 
-    # famID = property(get_famID,set_famID)
     # pid = property(get_pID,set_pID)
     # fatID = property(get_fatID,set_fatID)
     # famID = property(get_famID,set_famID)
