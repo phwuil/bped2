b1fce9671a470237f0a1e27a65cb5e8aeb881871
drkruzz <valentin.colliard@gmail.com>
2020-04-22 10:35:39 +0200

    add proportion.py

--- cplex/LBP_proportion.py
+++ cplex/LBP_proportion.py
@@ -15,14 +15,18 @@ nb_Gen_Max = [3,3,4,4,4,4,4,5,5,6,6,6,6,6]
 nb_Gen_Min = [math.ceil(x/2) for x in nb_Gen_Max]
 cl = 3
 
+data = np.zeros((len(nb_people),5),dtype=float)
+
 mean_gen_diff = []
 errorValues_diff = []
 max_diff = []
 min_diff = []
-
+w = 0
 for p,g_max,g_min in zip(nb_people,nb_Gen_Max,nb_Gen_Min):
+
     tab_diff = np.zeros(nb_ped)
     for nb in range(nb_ped):
+        y = 0
         nbChild = random.randint(6,12)
         g = random.randint(g_min,g_max)
         ped = Pedigree()
@@ -43,42 +47,40 @@ for p,g_max,g_min in zip(nb_people,nb_Gen_Max,nb_Gen_Min):
             p1 = ie1.posterior(f'X{i}')
             p2 = ie2.posterior(f'X{i}')
             x = [abs(p1[0] - p2[0]),abs(p1[1] - p2[1]),abs(p1[2] - p2[2]),abs(p1[3] - p2[3])]
-            error_lbp_laz.append(max(x))
+            v = max(x)
+            if v < 0.01:
+                data[w][0] += 100/(nb_ped*p)
+            elif v < 0.02:
+                data[w][1] += 100/(nb_ped*p)
+            elif v < 0.05:
+                data[w][2] += 100/(nb_ped*p)
+            elif v < 0.4:
+                data[w][3] += 100/(nb_ped*p)
+            else:
+                data[w][4] += 100/(nb_ped*p)
 
-        error_lbp_laz = np.array(error_lbp_laz)
-        tab_diff[nb] = error_lbp_laz.max()
+            #error_lbp_laz.append(max(x))
 
-    max_diff.append(tab_diff.max())
-    min_diff.append(tab_diff.min())
-    errorValues_diff.append(tab_diff.std())
-    mean_gen_diff.append(tab_diff.mean())
+        #error_lbp_laz = np.array(error_lbp_laz)
+        # tab_diff[nb] = error_lbp_laz.max()
+        #v = error_lbp_laz.max()
 
-f1 = plt.figure(1)
-plt.errorbar(nb_people, mean_gen_diff, yerr = errorValues_diff, ecolor='red')
-plt.title('Temps de calcul de l\'inférence d\'un BN compact en fonction de la taille du pedigree avec LBP')
-plt.xlabel('Taille du pedigree')
-plt.ylabel('Différence')
-plt.savefig('../cplex/figure/LBP/Différence de valeur d\'inférence entre LP et LBP en fonction de la taille du pedigree avec ecart-type')
-f1.show()
 
-f2 = plt.figure(2)
-plt.plot(nb_people, mean_gen_diff, label='mean')
-plt.plot(nb_people, max_diff, label='max')
-plt.plot(nb_people, min_diff, label='min')
-plt.legend(['mean','max','min'])
-plt.title('Temps de calcul de l\'inférence d\'un BN compact en fonction de la taille du pedigree avec LBP')
-plt.xlabel('Taille du pedigree')
-plt.ylabel('Différence')
-plt.savefig('../cplex/figure/LBP/Différence de valeur d\'inférence moyenne entre LP et LBP, min et max en fonction de la taille du pedigree')
-f2.show()
+        y+=1
+    # max_diff.append(tab_diff.max())
+    # min_diff.append(tab_diff.min())
+    # errorValues_diff.append(tab_diff.std())
+    # mean_gen_diff.append(tab_diff.mean())
+
+    w+=1
 
 # les tailles des graphes
 #columns = ('1000', '1500', '2000', '2500', '5000')
 
 columns = nb_people
 # les seuils testés
-values=[80,50,20,10,5]
-
+#values=[80,50,20,10,5]
+values=[50,40,5,2,1]
 # data = données à produire
 # en supposant qu'il y a
 # dans le cas N=1000 :
@@ -90,11 +92,11 @@ values=[80,50,20,10,5]
 #   - 36% entre 0.1 et 0.2
 #   - 20% entre 0.2 et 0.5
 #   -  4% >0.5
-data = np.array([[ 80, 20,  0,  0, 0],
-                 [ 60, 30, 10,  0, 0],
-                 [ 40, 32, 20,  8, 0],
-                 [ 25, 36, 24, 10, 5],
-                 [  8, 32, 36, 20, 4]])
+# data = np.array([[ 80, 20,  0,  0, 0],
+#                  [ 60, 30, 10,  0, 0],
+#                  [ 40, 32, 20,  8, 0],
+#                  [ 25, 36, 24, 10, 5],
+#                  [  8, 32, 36, 20, 4]])
 data=data.transpose()
 rows = ['<=%3.2f' % (x/100) for x in values]
 
@@ -112,7 +114,7 @@ cell_text = []
 for row in range(n_rows):
     plt.bar(index, data[row], bar_width, bottom=y_offset, color=colors[row])
     y_offset = y_offset + data[row]
-    cell_text.append(['%1.1f' % x for x in data[row]])
+    cell_text.append(['%1.1f' % x if x!=0 else "" for x in data[row]])
 # Reverse colors and text labels to display the last value at the top.
 colors = colors[::-1]
 cell_text.reverse()
@@ -130,5 +132,5 @@ plt.subplots_adjust(left=0.2, bottom=0.2)
 plt.ylabel("Proportion %")
 plt.xticks([])
 plt.title('Distribution des erreurs (en %)')
-
+plt.savefig('../cplex/figure/LBP/proportion')
 plt.show()
--- cplex/cplex_LBP.py
+++ cplex/cplex_LBP.py
@@ -10,8 +10,8 @@ import math
 
 f = 0.05
 nb_ped = 30
-nb_people = [10,20,50,100,200,300,500,1000,1500,2000,3000,4000,5000,7500,9000]
-nb_Gen_Max = [3,3,4,4,4,4,4,5,5,6,6,7,8,9,10]
+nb_people = [10,20,50,100,200,300,500,1000,1500,2000,2200,2400,3000,4000,5000,7500,9000]
+nb_Gen_Max = [3,3,4,4,4,4,4,5,5,6,6,6,6,7,8,9,10]
 # nb_people = [10,20,50,100,200,300,500,1000,1500,2000,2100,2200,2300,2400]
 # nb_Gen_Max = [3,3,4,4,4,4,4,5,5,6,6,6,6,6]
 nb_Gen_Min = [math.ceil(x/2) for x in nb_Gen_Max]
@@ -42,36 +42,36 @@ for p,g_max,g_min in zip(nb_people,nb_Gen_Max,nb_Gen_Min):
         t1 = process_time()
         lbp.doLBP(f'../cplex/bn/LBP/bn_compact_{p}_{g}_{nbChild}_{cl}_G{nb}.bif')
         t2 = process_time()
-
-        # t3 = process_time()
-        # laz.doLazyProg(f'../cplex/bn/LBP/bn_compact_{p}_{g}_{nbChild}_{cl}_G{nb}.bif')
-        # t4 = process_time()
-
         t2 = t2 - t1
-        # t4 = t4 - t3
-
         tab_lbp[nb] = t2
-        # tab_laz[nb] = t4
+        if p < 2500:
+            t3 = process_time()
+            laz.doLazyProg(f'../cplex/bn/LBP/bn_compact_{p}_{g}_{nbChild}_{cl}_G{nb}.bif')
+            t4 = process_time()
+            t4 = t4 - t3
+            tab_laz[nb] = t4
+        else:
+            tab_laz[nb] = None
 
     max_lbp.append(tab_lbp.max())
     min_lbp.append(tab_lbp.min())
     errorValues_lbp.append(tab_lbp.std())
     mean_gen_lbp.append(tab_lbp.mean())
 
-    # max_laz.append(tab_laz.max())
-    # min_laz.append(tab_laz.min())
-    # errorValues_laz.append(tab_laz.std())
-    # mean_gen_laz.append(tab_laz.mean())
+    max_laz.append(tab_laz.max())
+    min_laz.append(tab_laz.min())
+    errorValues_laz.append(tab_laz.std())
+    mean_gen_laz.append(tab_laz.mean())
 
-# f1 = plt.figure(1)
-# plt.errorbar(nb_people, mean_gen_lbp, yerr = errorValues_lbp, ecolor='red')
-# plt.errorbar(nb_people, mean_gen_laz, yerr = errorValues_laz, ecolor='green')
-# plt.legend(['lbp','lazy'])
-# plt.title('Temps de calcul de l\'inférence d\'un BNC avec LBP et LP')
-# plt.xlabel('Taille du pedigree')
-# plt.ylabel('Temps en sec')
-# plt.savefig('../cplex/figure/LBP/Temps de calcul de l\'inférence d\'un BN compact en fonction de la taille du pedigree avec ecart-type')
-# f1.show()
+f1 = plt.figure(1)
+plt.errorbar(nb_people, mean_gen_lbp, yerr = errorValues_lbp, ecolor='red')
+plt.errorbar(nb_people, mean_gen_laz, yerr = errorValues_laz, ecolor='green')
+plt.legend(['lbp','lazy'])
+plt.title('Temps de calcul de l\'inférence d\'un BNC avec LBP et LP')
+plt.xlabel('Taille du pedigree')
+plt.ylabel('Temps en sec')
+plt.savefig('../cplex/figure/LBP/Temps de calcul de l\'inférence d\'un BN compact en fonction de la taille du pedigree avec ecart-type')
+f1.show()
 
 # x = nb_ped * len(nb_people)
 # tmp = nb_people.copy()
@@ -168,21 +168,21 @@ for p,g_max,g_min in zip(nb_people,nb_Gen_Max,nb_Gen_Min):
 # errorValues_lbp.append(0)
 #
 #
-f1 = plt.figure(1)
-plt.plot(nb_people, mean_gen_lbp, label='mean')
-plt.plot(nb_people, max_lbp, label='max')
-plt.plot(nb_people, min_lbp, label='min')
-plt.legend(['mean','max','min'])
-plt.title('Temps de calcul de l\'inférence d\'un BNC en fonction du pedigree avec LBP')
-plt.xlabel('Taille du pedigree')
-plt.ylabel('Temps en sec')
-plt.savefig('../cplex/figure/LBP/Temps de calcul de l\'inférence moyenne d\'un BNC, min et max en fonction du pedigree')
-f1.show()
-
-f2 = plt.figure(2)
-plt.errorbar(nb_people, mean_gen_lbp, yerr = errorValues_lbp, ecolor='red')
-plt.title('Temps de calcul de l\'inférence d\'un BNC en fonction du pedigree avec LBP')
-plt.xlabel('Taille du pedigree')
-plt.ylabel('Temps en sec')
-plt.savefig('../cplex/figure/LBP/Temps de calcul de l\'inférence d\'un BNC en fonctiondu pedigree avec ecart-type')
-f2.show()
+# f1 = plt.figure(1)
+# plt.plot(nb_people, mean_gen_lbp, label='mean')
+# plt.plot(nb_people, max_lbp, label='max')
+# plt.plot(nb_people, min_lbp, label='min')
+# plt.legend(['mean','max','min'])
+# plt.title('Temps de calcul de l\'inférence d\'un BNC en fonction du pedigree avec LBP')
+# plt.xlabel('Taille du pedigree')
+# plt.ylabel('Temps en sec')
+# plt.savefig('../cplex/figure/LBP/Temps de calcul de l\'inférence moyenne d\'un BNC, min et max en fonction du pedigree')
+# f1.show()
+#
+# f2 = plt.figure(2)
+# plt.errorbar(nb_people, mean_gen_lbp, yerr = errorValues_lbp, ecolor='red')
+# plt.title('Temps de calcul de l\'inférence d\'un BNC en fonction du pedigree avec LBP')
+# plt.xlabel('Taille du pedigree')
+# plt.ylabel('Temps en sec')
+# plt.savefig('../cplex/figure/LBP/Temps de calcul de l\'inférence d\'un BNC en fonctiondu pedigree avec ecart-type')
+# f2.show()
